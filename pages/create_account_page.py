fab6203f008aa747604247c4b11e8967815a94d7
agnieszkasankowska9 <agnieszkasankowska9@gmail.com>
2026-03-16 11:38:54 +0000

    small fix

--- pages/create_account_page.py
+++ pages/create_account_page.py
@@ -1,10 +1,9 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-from time import sleep
-from utils.custom_types import Gender
-from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from utils.custom_types import Gender
+from pages.base_page import BasePage
 
 
 class Locators:
@@ -12,21 +11,22 @@ class Locators:
     CreateAccountPage locators
     """
     FIRST_NAME = (By.ID, "customer_firstname")
+    LAST_NAME = (By.ID, "customer_lastname")
     GENDER_MALE = (By.XPATH, '//label[@for="id_gender1"]')
     GENDER_FEMALE = (By.XPATH, '//label[@for="id_gender2"]')
     EMAIL = (By.ID, 'email')
     PASSWORD = (By.ID, 'passwd')
-    BIRTH_DAY_SELECT = (By.ID, "days")
-    BIRTH_MONTH_SELECT = (By.ID, "months")
-    BIRTH_YEAR_SELECT = (By.ID, "years")
-    REGISTER_BTN = (By.ID, "submitAccount")
+    REGISTER_BTN = (By.ID, 'submitAccount')
+    BIRTH_DAY_SELECT = (By.ID, 'days')
+    BIRTH_MONTH_SELECT = (By.ID, 'months')
+    BIRTH_YEAR_SELECT = (By.ID, 'years')
     VISIBLE_ERRORS = (By.XPATH, '//div[@class="alert alert-danger"]/ol/li')
     NUMBER_VISIBLE_ERRORS = (By.XPATH, '//div[@class="alert alert-danger"]/p')
 
 
 class CreateAccountPage(BasePage):
     """
-    CreateAccountPage Objects
+    Create Account Page Object
     """
     def choose_gender(self, gender):
         """
@@ -37,34 +37,27 @@ class CreateAccountPage(BasePage):
         else:
             self.driver.find_element(*Locators.GENDER_FEMALE).click()
 
-
     def enter_first_name(self, first_name):
+        """
+        Enter First Name
+        """
         self.driver.find_element(*Locators.FIRST_NAME).send_keys(first_name)
 
-
-    def get_entered_email(self):
+    def enter_last_name(self, last_name):
         """
-        email entered on previous page
-        :return:
+        Enter Last Name
         """
-        return self.driver.find_element(*Locators.EMAIL).get_attribute("value")
+        self.driver.find_element(*Locators.LAST_NAME).send_keys(last_name)
 
     def enter_password(self, password):
         """
-        type password
+        Enter Password
         """
         self.driver.find_element(*Locators.PASSWORD).send_keys(password)
 
-    def _verify_page(self):
-        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(Locators.FIRST_NAME))
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(Locators.REGISTER_BTN))
-
-
-
     def select_date_of_birth(self, date_of_birth):
         """
-        selectin date of birth
-        :return:
+        Select Date of Birth
         """
         birth_day = Select(self.driver.find_element(*Locators.BIRTH_DAY_SELECT))
         birth_day.select_by_value(str(date_of_birth.day))
@@ -73,21 +66,35 @@ class CreateAccountPage(BasePage):
         birth_year = Select(self.driver.find_element(*Locators.BIRTH_YEAR_SELECT))
         birth_year.select_by_value(str(date_of_birth.year))
 
+    def click_register_button(self):
+        """
+        Clicks Register Button
+        """
+        self.driver.find_element(*Locators.REGISTER_BTN).click()
+
+    def get_entered_email(self):
+        """
+        Get Email entered on previous page
+        """
+        return self.driver.find_element(*Locators.EMAIL).get_attribute("value")
+
+    def get_number_of_errors_message(self):
+        """
+        Get Number of Errors message
+        """
+        return self.driver.find_element(*Locators.NUMBER_VISIBLE_ERRORS).text
+
     def get_visible_errors(self):
         """
         Return all visible errors
         """
-        errors_webelements = self.driver.find_element(*Locators.VISIBLE_ERRORS)
+        errors_webelements = self.driver.find_elements(*Locators.VISIBLE_ERRORS)
         visible_errors = []
         for error in errors_webelements:
             visible_errors.append(error.text)
         return visible_errors
 
-    def get_number_of_errors_message(self):
-        return self.driver.find_element(*Locators.NUMBER_VISIBLE_ERRORS).text
 
-    def click_register_button (self):
-        self.driver.find_element(*Locators.REGISTER_BTN).click()
-
-
-
+    def _verify_page(self):
+        WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(Locators.FIRST_NAME))
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(Locators.REGISTER_BTN))
